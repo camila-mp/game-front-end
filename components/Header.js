9f3d9cddf8310879e08f12cae570831730ad3598
camila-mp <ca.malvp@gmail.com>
2022-05-16 15:12:58 -0300

    component Header: add links for desktop screen and correct small details

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../public/Assets/logo.png";
+import logodesk from "../public/Assets/logo-desktop.png";
 import icon from "../public/Assets/icon.png";
 import Image from 'next/image';
 
@@ -8,20 +8,24 @@ const Header = () => {
     <header className="header">
       <div className="logo-container">
         <Image
-          className="logo"
-          src={logo}
+          className="logo-desk"
+          src={logodesk}
           alt="PUBG BATTLEGROUND logo"
         />
       </div>
+      <nav className="nav-desktop">
+        <a className="nav-link" href="www.google.com">about the event</a>
+        <a className="nav-link" href="www.google.com">rewards</a>
+        <a className="nav-link" href="www.google.com">account</a>
+      </nav>
       <nav className="nav">
-        <button className="nav-button">LOGIN</button>
+        <button className="nav-button">login</button>
         <div className="icon-container">
           <Image
             src={icon}
             alt="globe icon"
           />
         </div>
-        
       </nav>
     </header>
   )
