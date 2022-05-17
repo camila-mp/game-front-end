967e12e22a5ec75bb5b459795382d7a79bb05f57
camila-mp <ca.malvp@gmail.com>
2022-05-17 15:30:55 -0300

    Hero component: add images and button

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,34 @@
 import React from "react";
 import Image from 'next/image';
 import spray from '../public/Assets/spray-bg.png';
+import vikendi from '../public/Assets/VikendiWonderlandLogo.png';
+import vikendi_desk from '../public/Assets/VikendiWonderlandLogo_desk.png';
 
 const Hero = () => {
   return (
     <section className="hero-container">
+      <div className="spray-wrapper">
+        <Image
+          className="spray"
+          src={spray}
+          alt="background spray"
+        />
+      </div>
+      <div className="vikendi-wrapper">
+        <Image
+          className="vikendi"
+          src={vikendi}
+          alt="vikendi logo"
+        />
+      </div>
+      <div className="vikendi-wrapper-desk">
       <Image
-        className="spray"
-        src={spray}
-        alt="background spray"
-      />
+          className="vikendi"
+          src={vikendi_desk}
+          alt="vikendi logo"
+        />
+      </div>
+      <button className="hero-button">login or create account</button>
     </section>
   )
 }
