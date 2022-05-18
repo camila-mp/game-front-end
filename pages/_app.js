6c0eaa029f213c6c48bcbecfa47502a3a8bb24b5
camila-mp <ca.malvp@gmail.com>
2022-05-17 22:19:59 -0300

    _app: add imports aboutEvent.css and loginOrCreateButton.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import '../styles/header.css'
 import '../styles/hero.css'
+import '../styles/aboutEvent.css'
+import '../styles/loginOrCreateButton.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
