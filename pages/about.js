43ad6d9d28f11d1337ea0e7abfe74aa991a0d013
camila-mp <ca.malvp@gmail.com>
2022-05-18 10:10:13 -0300

    create new page 'About' and put 'AboutArticle' component inside

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutArticle from "../components/AboutArticle";
+
+const About = () => {
+  return (
+    <section className="about-page">
+      <AboutArticle/>
+    </section>
+  )
+}
+
+export default About;
