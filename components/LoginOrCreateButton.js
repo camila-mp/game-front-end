31c5b2e3e4c4a38192d2d3bce4ab04fea8fcff34
camila-mp <ca.malvp@gmail.com>
2022-05-17 22:18:34 -0300

    create LoginOrCreateButton component and css

---
+++ components/LoginOrCreateButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginOrCreateButton = () => {
+  return (
+    <div>
+      <button className="login-create-button">login or create account</button>
+    </div>
+  )
+}
+
+export default LoginOrCreateButton;
