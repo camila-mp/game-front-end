e07828ce9847d15c50af69b0aa4ab466a8be1cf2
camila-mp <ca.malvp@gmail.com>
2022-05-18 10:08:52 -0300

    rename 'AboutEvent' component to 'AboutArticle'

---
+++ components/AboutArticle.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from 'next/image';
+import grad_line from '../public/Assets/grad-line.png' 
+import LoginOrCreateButton from "./LoginOrCreateButton";
+
+const AboutArticle = () => {
+  return (
+    <article className="about-event-container">
+      <h6>vikendi wonderland giveaway</h6>
+      <h2>about the event</h2>
+      <div className="gradline-wrapper">
+        <Image
+          src={grad_line}
+          alt="separating line"
+        />
+      </div>
+      <p className="about-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pulvinar lacinia nisl quis mollis. Curabitur fringilla euismod arcu, sit amet maximus mauris sodales id. Aenean at consectetur turpis. Phasellus semper nisi eget eros rhoncus faucibus pharetra.
+      </p>
+      <p className="about-text">
+        Phasellus ullamcorper, mauris rhoncus faucibus pharetra, est tellus mattis, nec congue erat tellus non nibh. Mauris euismod elementum tellus. Nulla eleifend eros vel purus facilisis sodales.
+      </p>
+      <div className="rules-div">
+        <a className="rules-link" href="https://github.com/">see the official rules</a>
+      </div>
+      <LoginOrCreateButton/>
+    </article>
+  )
+}
+
+export default AboutArticle;
