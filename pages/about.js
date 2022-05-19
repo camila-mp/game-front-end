c7ce30e68d85d7f093d328d747a4c767fcf688fb
camila-mp <ca.malvp@gmail.com>
2022-05-19 14:28:01 -0300

    component About: insert About Characters; create style

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutArticle from "../components/AboutArticle";
+import AboutCharacters from "../components/AboutCharacters";
 
 const About = () => {
   return (
     <section className="about-page">
       <AboutArticle/>
+      <AboutCharacters/>
     </section>
   )
 }
