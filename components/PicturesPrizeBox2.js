9d567edd96535018754c65205d7b2f9db3a0aa1e
camila-mp <ca.malvp@gmail.com>
2022-05-23 16:05:41 -0300

    create PicturesPrizeBox2 component

---
+++ components/PicturesPrizeBox2.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from 'next/image';
+import prize_coupons_large from '../public/Assets/prize_coupons_large.png';
+import nickel_box_large from '../public/Assets/nickel_box_large.png';
+import plus_sign from '../public/Assets/plus_sign.png';
+
+const PicturesPrizeBox2 = () => {
+  return (
+    <section className="pictures-container">
+      <div className='picture coupons-large'>
+        <Image
+          src={prize_coupons_large}
+          alt='contraband coupons'
+        />
+      </div>
+      <div className='picture nickel-box-large'>
+        <Image
+          src={nickel_box_large}
+          alt='nickel box'
+        />
+      </div>
+      <div className='picture plus-sign'>
+        <Image
+          src={plus_sign}
+          alt='plus sign'
+        />
+      </div>
+    </section>
+  )
+}
+
+export default PicturesPrizeBox2;
