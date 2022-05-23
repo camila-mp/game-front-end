ccb6dfc1116cf9d62c0149519f349e139cacf1b9
camila-mp <ca.malvp@gmail.com>
2022-05-23 16:46:15 -0300

    improving logic to render picturesPrizeBox components inside PrizeBox

--- components/PrizeBox.js
+++ components/PrizeBox.js
@@ -2,22 +2,19 @@ import React from "react";
 import Image from 'next/image';
 import spray_yellow from '../public/Assets/img-spraymask-yellow.png';
 import spray_blue from '../public/Assets/img-spraymask-blue.png';
-import PicturesPrizeBox1 from "./PicturesPrizeBox1";
-import PicturesPrizeBox2 from "./PicturesPrizeBox2";
 
-const PrizeBox = ({ title, prizes, color }) => {
-
+const PrizeBox = ({ title, prizes, sprayColor, children }) => {
   return (
     <section className="box">
       <div className="spray-wrapper-r">
         <Image
-          src={color === 'blue' ? spray_blue : spray_yellow}
+          src={sprayColor === 'blue' ? spray_blue : spray_yellow}
           alt="spray picture behind title"
         />
       </div>
       <h6 className="prize-title">{ title }</h6>
       { 
-        color === 'blue' ? <PicturesPrizeBox2/> : <PicturesPrizeBox1/>
+        children
       }
       <p>+++++ will receive {'>>>>>>>'}</p>
       <h6 className="prizes">{ prizes }</h6>
