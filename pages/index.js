bdd9f10dfb7ffe5cb32548a9aaa0342cc1ed962a
camila-mp <ca.malvp@gmail.com>
2022-05-24 21:50:53 -0300

    add steps to index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/Header.js';
 import Hero from '../components/Hero';
 import About from './about';
 import Rewards from './rewards';
+import Steps from './steps';
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Hero/>
         <About/>
         <Rewards/>
+        <Steps/>
       </main>
     </div>
   )
