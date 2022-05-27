f0abfc5b0058fe104839e9a72b1fbe435de8357a
camila-mp <ca.malvp@gmail.com>
2022-05-27 17:20:53 -0300

    AboutArticle: replace image 'grad-line' that was causing error with a div

--- components/AboutArticle.js
+++ components/AboutArticle.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Image from 'next/image';
-import grad_line from '../public/Assets/grad-line.png' 
 import LoginOrCreateButton from "./LoginOrCreateButton";
 
 const AboutArticle = () => {
@@ -8,12 +6,7 @@ const AboutArticle = () => {
     <article className="about-event-container">
       <h6>vikendi wonderland giveaway</h6>
       <h2>about the event</h2>
-      <div className="gradline-wrapper">
-        <Image
-          src={grad_line}
-          alt="separating line"
-        />
-      </div>
+      <div className="gradline"/>
       <p className="about-text">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pulvinar lacinia nisl quis mollis. Curabitur fringilla euismod arcu, sit amet maximus mauris sodales id. Aenean at consectetur turpis. Phasellus semper nisi eget eros rhoncus faucibus pharetra.
       </p>
